318375ee5991585e3d8160cbc8263a53e0b1b4da
BreakThemBalls <114090284+BreakThemBalls@users.noreply.github.com>
2022-09-23 14:51:34 +0800

    Brick Demolisher Game

--- src/Brickdemolisher/Gameplay.java
+++ src/Brickdemolisher/Gameplay.java
@@ -2,7 +2,9 @@ package Brickdemolisher;
 // I imported that is needed for graphics and the JPanel which is the frame of the app
 import java.awt.Color;
 import java.awt.Graphics;
-import javax.swing.JPanel;
+import java.awt.Image;
+import java.awt.Toolkit;
+import javax.swing.JPanel;    
 // These 4 imports are for the controls
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -26,6 +28,9 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
     private int ballXdir = -1;
     private int ballYdir = -2;
     
+    private MapGenerator map;
+    Image img = Toolkit.getDefaultToolkit().createImage("/Users/johnm/Downloads/Mario.jpg");
+    
     public Gameplay() {
         addKeyListener(this);
         setFocusable(true);
@@ -37,18 +42,23 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
     @Override
     public void paint(Graphics g) {
         // insert the background
-        
+        g.setColor(Color.black);
+        g.fillRect(1,1, 692, 592);
+        g.drawImage(img,40,40,null);
         
         // insert the border
-        
+        g.setColor(Color.gray);
+        g.fillRect(0, 0, 15, 592);
+        g.fillRect(0, 0, 692, 15);
+        g.fillRect(691, 0, 15, 592);
         
         // the paddle
-        g.setColor(Color.green);
-        g.fillRect(playerX, 550, 100, 8);
+        g.setColor(Color.white);
+        g.fillRect(playerX, 545, 115, 10);
         
         // insert the ball
-        
-        
+        g.setColor(Color.white);
+        g.fillOval(ballposX,ballposY,20,20);
         
         g.dispose();
     }
@@ -91,5 +101,11 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
     public void moveLeft() {
         play = true;
         playerX -= 20;
+    }
+
+    private static class MapGenerator {
+
+        public MapGenerator() {
+        }
     }
 }
